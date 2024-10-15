4176d8d7312702b50534a9818a787df1e635d833
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-10-15 11:38:16 +0200

    feat(initialization): Add cord protection

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-import http from 'http';
+import 'dotenv/config';
 import express from 'express';
+import { createServer } from 'http';
 import { Server } from 'socket.io';
+import routes from './routes';
 import { initializeSockets } from './sockets';
+import cors from 'cors';
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
-});
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: process.env.CORS_ORIGIN } });
+
+app.use(express.json());
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use('/', routes);
 
 initializeSockets(io);
 
 const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, () => {
-  console.log(`Socket-io server running on port ${PORT}.`);
+  console.log(`Server running on http://localhost:${PORT}.`);
 });
