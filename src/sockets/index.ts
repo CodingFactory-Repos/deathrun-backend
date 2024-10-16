b31f3e71b0fe76c611cdff051bef08a363a665ae
AyakorK <guillaume.moret@yahoo.com>
2024-10-16 12:57:08 +0200

    lint: Add a type on my variables

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,38 +1,36 @@
 import { Server } from 'socket.io';
 
 export const initializeSockets = (io: Server) => {
+  const port: number = 11100; // Port number
+  const io2: Server = require('socket.io')(); // Socket.IO server instance
 
-  const port = 11100;
-  const io2 = require('socket.io')();
-
-  io2.use((socket, next) => {
+  io2.use((socket: any, next: Function) => {
+    // Middleware for token authentication
     if (socket.handshake.query.token === "UNITY") {
-        next();
+      next();
     } else {
-        next(new Error("Authentication error"));
+      next(new Error("Authentication error"));
     }
   });
 
-  
-  io2.on('connection', socket => {
-    // Whatever you're receiving from Unity just print it
+  io2.on('connection', (socket: any) => {
     if (socket.handshake.query.token === "UNITY") {
       console.log("Unity connected");
     }
-    socket.emit('connection', {date: new Date().getTime(), data: "Hello Unity"})
-  
-    socket.on('hello', (data) => {
-      socket.emit('hello', {date: new Date().getTime(), data: data});
+
+    socket.emit('connection', { date: new Date().getTime(), data: "Hello Unity" });
+
+    socket.on('hello', (data: any) => {
+      socket.emit('hello', { date: new Date().getTime(), data: data });
     });
-  
+
     socket.on('playerMovement', (data: Object) => {
-      console.log("Player Position is: " + data);
-      
-      // socket.emit('class', {date: new Date().getTime(), data: data});
+      console.log("Player Position is: " + JSON.stringify(data));
+      // socket.emit('class', { date: new Date().getTime(), data: data });
     });
   });
-  
+
   io2.listen(port);
   console.log('listening on *:' + port);
-  console.log("Full URL of the io2 is " + io2.httpServer.address().address + ":" + io2.httpServer.address().port);
+  console.log("Full URL of the io2 is " + io2.httpServer.address()?.address + ":" + io2.httpServer.address()?.port);
 };
