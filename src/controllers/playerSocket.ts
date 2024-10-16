a190b3001d0f109b85a391c52ecde27813453278
AyakorK <guillaume.moret@yahoo.com>
2024-10-16 16:40:42 +0200

    feat: Send positions from back-end to front-end

--- src/controllers/playerSocket.ts
+++ src/controllers/playerSocket.ts
@@ -1,8 +1,19 @@
 import { Socket } from 'socket.io';
 
 export const playerSocket = (socket: Socket) => {
-  socket.on('players:move', (msg) => {
-    console.log('players:move' + msg);
-    socket.broadcast.emit('players:move', msg);
+  socket.on('players:move', (positionXYZ) => {
+    const positionSanitized = sanitize(positionXYZ);
+    const position = positionSanitized.split(',');
+    
+    const positionXY = {
+      x: parseFloat(position[0]),
+      y: parseFloat(position[1]),
+    };
+    socket.broadcast.emit('players:move', positionXY);
   });
 };
+
+function sanitize(position: string) {
+  const sanitizedPosition = position.replace(/[() ]/g, '');
+  return sanitizedPosition
+}
