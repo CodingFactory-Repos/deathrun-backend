73ac23cf250e8ad6cfcccd18e22d88ab26572197
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-10-16 14:49:51 +0200

    Squashed commit of the following:
    commit 7828fabb3ede4c88760d50f82fce282bf31469f2
    Author: Loule | Louis <35641311+Loule95450@users.noreply.github.com>
    Date:   Wed Oct 16 14:25:29 2024 +0200
    
        Feature/rooms (#6)
    
        * feat(rooms): Create MongoDB BDD with docker
    
        * feat(rooms): Delete unused sockets and create roomsockets
    
        * fix(bdd): Remove mongo-data from git
    
        * fix(bdd): Create init file for mongo

---
+++ init-mongo.js
@@ -0,0 +1,2 @@
+db = db.getSiblingDB('database'); // Création de la base de données "database"
+db.createCollection('rooms'); // Création de la collection "rooms"
--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,36 +1,13 @@
-import { Server } from 'socket.io';
+import { roomSocket } from "../controllers/roomSocket";
+import {disconnectedSocket} from "../controllers/disconnectedSocket";
+import {Server} from "socket.io";
 
 export const initializeSockets = (io: Server) => {
-  const port: number = 11100; // Port number
-  const io2: Server = require('socket.io')(); // Socket.IO server instance
+  io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
 
-  io2.use((socket: any, next: Function) => {
-    // Middleware for token authentication
-    if (socket.handshake.query.token === "UNITY") {
-      next();
-    } else {
-      next(new Error("Authentication error"));
-    }
-  });
+    roomSocket(socket);
 
-  io2.on('connection', (socket: any) => {
-    if (socket.handshake.query.token === "UNITY") {
-      console.log("Unity connected");
-    }
-
-    socket.emit('connection', { date: new Date().getTime(), data: "Hello Unity" });
-
-    socket.on('hello', (data: any) => {
-      socket.emit('hello', { date: new Date().getTime(), data: data });
+    disconnectedSocket(socket);
     });
-
-    socket.on('playerMovement', (data: Object) => {
-      console.log("Player Position is: " + JSON.stringify(data));
-      // socket.emit('class', { date: new Date().getTime(), data: data });
-    });
-  });
-
-  io2.listen(port);
-  console.log('listening on *:' + port);
-  console.log("Full URL of the io2 is " + io2.httpServer.address()?.address + ":" + io2.httpServer.address()?.port);
 };
