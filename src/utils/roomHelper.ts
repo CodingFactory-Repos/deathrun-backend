61d806059b3781fae61cc3d431e7f06c6a582fe6
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-10-17 15:12:32 +0200

    fix(traps): Check if player is logged in room

---
+++ src/utils/roomHelper.ts
@@ -0,0 +1,23 @@
+import { Socket } from 'socket.io';
+import { clientDB } from './databaseHelper';
+
+export const checkUserInRoom = async (socket: Socket): Promise<{ room: string, role: 'player' | 'god' } | false> => {
+  const playerRoom = await clientDB.collection('rooms').findOne({ players: socket.id });
+  if (playerRoom) {
+    return { room: playerRoom.code, role: 'player' };
+  }
+
+  const godRoom = await clientDB.collection('rooms').findOne({ gods: socket.id });
+  if (godRoom) {
+    return { room: godRoom.code, role: 'god' };
+  }
+
+  return false;
+};
+
+export const isPlayer = async (socket: Socket): Promise<boolean> => {
+    const playerRoom = await checkUserInRoom(socket);
+    if (playerRoom) return playerRoom.role === 'player';
+
+    return false;
+}
