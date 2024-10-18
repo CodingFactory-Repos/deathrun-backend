d79867f6799b5bfa49e04d77b233aa03ba936338
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-10-18 11:20:51 +0200

    style(init-mongo.js, playerSocket.ts, src/index.ts): minor formatting and indentation adjustments

--- src/controllers/playerSocket.ts
+++ src/controllers/playerSocket.ts
@@ -1,20 +1,18 @@
-import { Socket } from 'socket.io';
-// import {checkUserInRoom, isPlayer} from "../utils/roomHelper";
+import {Socket} from 'socket.io';
+import {checkUserInRoom, isPlayer} from "../utils/roomHelper";
 
 export const playerSocket = (socket: Socket) => {
-  socket.on('players:move', async (positionXYZ) => {
-    // const user = await checkUserInRoom(socket);
-    // if (!user) return socket.emit('error', 'You are not in a room');
-    // if (!await isPlayer(socket)) return socket.emit('error', 'You are not a player');
+    socket.on('players:move', async (positionXYZ) => {
+        const user = await checkUserInRoom(socket);
+        if (!user) return socket.emit('error', 'You are not in a room');
+        if (!await isPlayer(socket)) return socket.emit('error', 'You are not a player');
 
-    const [x, y] = sanitize(positionXYZ).split(',').map(parseFloat);
+        const [x, y] = sanitize(positionXYZ).split(',').map(parseFloat);
 
-    // socket.to(user.room).broadcast.emit('players:move', { x, y });
-    socket.broadcast.emit('players:move', { x, y });
-
-  });
+        socket.to(user.room).broadcast.emit('players:move', { x, y });
+    });
 };
 
 function sanitize(position: string) {
-  return position.replace(/[() ]/g, '');
+    return position.replace(/[() ]/g, '');
 }
