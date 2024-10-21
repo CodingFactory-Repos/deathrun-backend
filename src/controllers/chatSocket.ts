c6e2a10831be3221b8fe3c5f390983e27f92d36e
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-10-21 11:17:54 +0200

    feat(chat): Update chatSocket.ts

--- src/controllers/chatSocket.ts
+++ src/controllers/chatSocket.ts
@@ -1,8 +1,14 @@
 import { Socket } from 'socket.io';
+import {checkUserInRoom, isPlayer} from "../utils/roomHelper";
 
 export const chatSocket = (socket: Socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('chat message: ' + msg);
-    socket.broadcast.emit('chat message', "Hello from the server! You said: " + msg);
+  socket.on('room:message', async (msg) => {
+    // Check if user is in a room
+    const user = await checkUserInRoom(socket);
+    if (!user) return socket.emit('error', 'You are not in a room');
+    if (!isPlayer(socket)) return socket.emit('error', 'You are not a god');
+
+    // Broadcast message to room
+    socket.to(user.room).broadcast.emit('room:message', msg);
   });
 };
