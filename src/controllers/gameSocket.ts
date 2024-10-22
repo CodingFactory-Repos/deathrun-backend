ae380b2a7df31355c212c63b68733508dcad4857
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-10-22 15:23:41 +0200

    feat(gameSocket.ts): update room data to include started status and add validation for started game

--- src/controllers/gameSocket.ts
+++ src/controllers/gameSocket.ts
@@ -3,22 +3,18 @@ import {checkUserInRoom} from "../utils/roomHelper";
 import {clientDB} from "../utils/databaseHelper";
 
 export const startGame = async (socket: Socket) => {
-    // Check if user is the creator of the room
     const user = await checkUserInRoom(socket);
     if (!user) return socket.emit('error', 'You are not in a room');
 
-    // Check if the room have at least 1 player, 1 god and started is false
-    let room = await clientDB.collection('rooms').findOne({code: user.room});
+    const room = await clientDB.collection('rooms').findOne({ code: user.room });
     if (!room) return socket.emit('error', 'Room not found');
     if (room.creator !== socket.id) return socket.emit('error', 'You are not the creator of the room');
     if (room.players.length < 1) return socket.emit('error', 'Room must have at least 1 player');
     if (room.gods.length < 1) return socket.emit('error', 'Room must have at least 1 god');
+    if (room.started) return socket.emit('error', 'Game already started');
 
-    // Set started to true
-    room = await clientDB.collection('rooms').updateOne({code: user.room}, {$set: {started: true}}).then(() => {
-        return clientDB.collection('rooms').findOne({code: user.room});
-    });
+    await clientDB.collection('rooms').updateOne({ code: user.room }, { $set: { started: true } });
+    const updatedRoom = await clientDB.collection('rooms').findOne({ code: user.room });
 
-    // Start game
-    socket.to(user.room).broadcast.emit('rooms:events', room);
+    socket.to(user.room).broadcast.emit('rooms:events', updatedRoom);
 }
