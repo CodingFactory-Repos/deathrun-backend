40b891411d03d27182c72e24cf300f7560cd27f7
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2024-10-22 11:51:52 +0200

    Feature/waiting room (#12)
    * fix: Fix the way of saving props and add a props socket
    
    * lint: Remove logs
    
    * fix: Fix rooms to handle trap and props error placements
    
    * fix: Fix the trap when they're not yet existing
    
    * fix: Handle errors correctly
    
    * fix(propsSocket.ts): add validation for props array format to prevent errors
    
    * fix(roomSocket.ts): fix room join logic to handle creator correctly
    
    ---------
    
    Co-authored-by: Loule | Louis <35641311+Loule95450@users.noreply.github.com>

--- src/controllers/roomSocket.ts
+++ src/controllers/roomSocket.ts
@@ -7,8 +7,8 @@ interface PropsCoordinates {
 }
 
 export const roomSocket = (socket: Socket) => {
-    socket.on('rooms:create', (coordinates: [string]) => {
-        createRoom(socket, coordinates);
+    socket.on('rooms:create', () => {
+        createRoom(socket);
     });
 
     socket.on('rooms:join', (msg: { code: string, joinAs: "player" | "god" }) => {
@@ -16,21 +16,13 @@ export const roomSocket = (socket: Socket) => {
     });
 };
 
-function createRoom(socket: Socket, data: [string]) {
-    if (data.length < 1) {
-        socket.emit('rooms:create', {error: 'Invalid coordinates'});
-        return;
-    }
-
-    const propsCoordinates: PropsCoordinates[] = convertToCoordinates(data);
-
+function createRoom(socket: Socket) {
     const roomCode = Math.random().toString(36).substring(7);
     clientDB.collection('rooms').insertOne({
         code: roomCode,
         creator: socket.id,
         players: [socket.id],
         gods: [],
-        props: propsCoordinates
     }).then(() => {
         return clientDB.collection('rooms').findOne({code: roomCode});
     }).then((result) => {
@@ -52,13 +44,15 @@ function joinRoom(socket: Socket, data: { code: string, joinAs: "player" | "god"
                 )
             }
 
-            await clientDB.collection('rooms').findOne({code: data.code}).then((updatedRoom) => {
-                socket.join(data.code);
-                socket.emit('rooms:join', updatedRoom);
+            await clientDB.collection('rooms').findOne({code: data.code}).then(async (updatedRoom) => {
+                if (updatedRoom && updatedRoom.creator) {
+                    socket.join(data.code); // Join the room
+                    socket.emit('rooms:join', updatedRoom); // Send the room data to the player
 
-                socket.to(data.code).emit('rooms:events', updatedRoom);
+                    socket.to(data.code).emit('rooms:events', updatedRoom); // Send the room data to the other players
 
-                console.log(socket.id + ' joined room ' + data.code);
+                    socket.to(updatedRoom.creator).emit('trapper:join', {player: socket.id}); // Send the player id to the creator
+                }
             });
         } else {
             socket.emit('rooms:join', {error: 'Room not found'});
@@ -92,7 +86,7 @@ export async function disconnectRoom(socket: Socket) {
             socket.to(godRoom.code).emit('rooms:events', updatedRoom);
         });
     }
-};
+}
 
 function convertToCoordinates(coordinates: [string]): PropsCoordinates[] {
     return coordinates.map((prop: string) => {
