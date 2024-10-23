16ced3d50dd85e70d85ad0e2aca70c8210432949
AyakorK <guillaume.moret@yahoo.com>
2024-10-23 12:38:53 +0200

    fix: Change players to players.id to match the object

--- src/utils/roomHelper.ts
+++ src/utils/roomHelper.ts
@@ -2,7 +2,7 @@ import { Socket } from 'socket.io';
 import { clientDB } from './databaseHelper';
 
 export const checkUserInRoom = async (socket: Socket): Promise<{ room: string, role: 'player' | 'god' } | false> => {
-  const playerRoom = await clientDB.collection('rooms').findOne({ players: socket.id });
+  const playerRoom = await clientDB.collection('rooms').findOne({ 'players.id': socket.id });
   if (playerRoom) {
     return { room: playerRoom.code, role: 'player' };
   }
