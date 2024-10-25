53aa02b8b77a2edac840dfa00575141b3ce619d5
AyakorK <guillaume.moret@yahoo.com>
2024-10-25 15:18:56 +0200

    feat: Add a mini-function to start the game

--- src/controllers/gameSocket.ts
+++ src/controllers/gameSocket.ts
@@ -1,5 +1,5 @@
 import {Socket} from "socket.io";
-import {checkUserInRoom} from "../utils/roomHelper";
+import {checkUserInRoom, isGod} from "../utils/roomHelper";
 import {clientDB} from "../utils/databaseHelper";
 
 export const startGame = async (socket: Socket) => {
@@ -8,7 +8,7 @@ export const startGame = async (socket: Socket) => {
 
     const room = await clientDB.collection('rooms').findOne({ code: user.room });
     if (!room) return socket.emit('error', 'Room not found');
-    if (room.creator !== socket.id) return socket.emit('error', 'You are not the creator of the room');
+    if (!isGod(room, socket)) return socket.emit('error', 'You are not the creator of the room');
     if (room.players.length < 1) return socket.emit('error', 'Room must have at least 1 player');
     if (room.gods.length < 1) return socket.emit('error', 'Room must have at least 1 god');
     if (room.started) return socket.emit('error', 'Game already started');
@@ -16,5 +16,6 @@ export const startGame = async (socket: Socket) => {
     await clientDB.collection('rooms').updateOne({ code: user.room }, { $set: { started: true } });
     const updatedRoom = await clientDB.collection('rooms').findOne({ code: user.room });
 
+    socket.to(room.creator).emit('rooms:start');
     socket.to(user.room).broadcast.emit('rooms:events', updatedRoom);
 }
