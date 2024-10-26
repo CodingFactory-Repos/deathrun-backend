9f4101a20f14f96db7e5fb2ebffc056f3f2d501a
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2024-10-26 21:04:25 +0200

    feat: Add the display of the game and send it to the frontend (#29)

---
+++ src/controllers/cameraSocket.ts
@@ -0,0 +1,22 @@
+import { Socket } from 'socket.io';
+import {checkUserInRoom, getRoom, isPlayer} from "../utils/roomHelper";
+
+export const cameraSocket = (socket: Socket) => {
+  socket.on('camera:request', async (frame) => {
+    
+    const user = await checkUserInRoom(socket);
+    if (!user) return socket.emit('error', 'You are not in a room');
+    if (!isPlayer(socket)) return socket.emit('error', 'You are not a god');
+
+    const room = await getRoom(user.room);
+    const roomCreator = room.creator;
+
+    if (roomCreator !== socket.id) return socket.emit('error', 'You are not the room creator');
+
+    const gods = room.gods.map((god: any) => god.id);
+
+    gods.forEach(god => {
+        socket.to(god).emit('camera:sending', frame);
+    });
+  });
+};
