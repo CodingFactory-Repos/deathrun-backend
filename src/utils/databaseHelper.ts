2a6aa008b54d015574fd0883dfcd2b4a260e6a44
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-10-28 14:24:20 +0100

    Fix(databaseHelper.ts): Update databaseHelper.ts

--- src/utils/databaseHelper.ts
+++ src/utils/databaseHelper.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb'
 
-const client = new MongoClient(process.env.MONGO_URI ?? '');
+const client = new MongoClient(process.env.MONGO_URI ?? 'mongodb://localhost:27017/');
 
 export const clientDB = client.db('database');
