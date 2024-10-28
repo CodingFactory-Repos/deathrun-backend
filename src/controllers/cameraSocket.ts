29805d759207b837b9d4a88fa4442c36abbb4f5a
AyakorK <guillaume.moret@yahoo.com>
2024-10-28 13:18:23 +0100

    feat: Create an instance of a worker for camera tasks

--- src/controllers/cameraSocket.ts
+++ src/controllers/cameraSocket.ts
@@ -1,5 +1,6 @@
 import { Socket } from 'socket.io';
-import {checkUserInRoom, getRoom, isPlayer} from "../utils/roomHelper";
+import { Worker } from 'worker_threads';
+import { checkUserInRoom, getRoom, isPlayer } from "../utils/roomHelper";
 
 export const cameraSocket = (socket: Socket) => {
   socket.on('camera:request', async (frame) => {
@@ -13,6 +14,23 @@ export const cameraSocket = (socket: Socket) => {
 
     if (roomCreator !== socket.id) return socket.emit('error', 'You are not the room creator');
 
-    socket.to(room.code).emit('camera:sending', frame);
+    const worker = new Worker('./src/controllers/worker.js', {
+      workerData: { frame, roomCode: room.code }
+    });
+
+    worker.on('message', (message) => {
+      socket.to(message.roomCode).emit('camera:sending', message.frame);
+    });
+
+    worker.on('error', (error) => {
+      console.error('Worker error:', error);
+      socket.emit('error', 'An error occurred while processing the frame');
+    });
+
+    worker.on('exit', (code) => {
+      if (code !== 0) {
+        console.error(`Worker stopped with exit code ${code}`);
+      }
+    });
   });
 };
