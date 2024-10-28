d36f23c32248accf46499de4447a378c3aab7735
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2024-10-28 13:00:40 +0100

    feat(src/controllers/trapSocket.ts): add trap to room using $push operator

--- src/controllers/trapSocket.ts
+++ src/controllers/trapSocket.ts
@@ -155,7 +155,7 @@ async function addTrapToRoom(socket: Socket, trap: Trap): Promise<boolean> {
         const buyTrapSuccess = await buyTrap(socket, trap);
         if (!buyTrapSuccess) return false;
 
-        await clientDB.collection('rooms').updateOne({ 'gods.id': socket.id }, { $push: { traps: trap } });
+        await clientDB.collection('rooms').updateOne({ 'gods.id': socket.id }, { $push: { traps: trap } as any });
         const updatedRoom = await clientDB.collection('rooms').findOne({ 'gods.id': socket.id });
         const room = await getRoom(socket);
         if (room?.traps) socket.emit('traps:list', room.traps);
